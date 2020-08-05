353e43cdce1120f88178f6c5e666f02d52a34dcb
GaneshDotsquares <techdotsquares@gmail.com>
2020-08-05 13:51:36 +0530

    finished UPQSDKTest

---
+++ UPQSDKTest/UPQSDKTest/Service.swift
@@ -0,0 +1,15 @@
+//
+//  Service.swift
+//  UPQSDKTest
+//
+//  Created by Ganesh on 05/08/20.
+//  Copyright © 2020 Dotsquares. All rights reserved.
+//
+
+import Foundation
+public class Service{
+    private init(){}
+    public static func doSomething()->String{
+        return "Do something"
+    }
+}
---
+++ UPQSDKTest/UPQSDKTest/UPQSDKTest.h
@@ -0,0 +1,19 @@
+//
+//  UPQSDKTest.h
+//  UPQSDKTest
+//
+//  Created by Ganesh on 05/08/20.
+//  Copyright © 2020 Dotsquares. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UPQSDKTest.
+FOUNDATION_EXPORT double UPQSDKTestVersionNumber;
+
+//! Project version string for UPQSDKTest.
+FOUNDATION_EXPORT const unsigned char UPQSDKTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UPQSDKTest/PublicHeader.h>
+
+
